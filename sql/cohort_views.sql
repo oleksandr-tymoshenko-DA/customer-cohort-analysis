97a6afd7ff19264b602cb466193ad1c2c56d4cb8
oleksandr-tymoshenko-DA <oleksandr.tymoshenko.da@gmail.com>
2026-01-17 18:07:45 +0200

    Add customer_first_order view

--- sql/cohort_views.sql
+++ sql/cohort_views.sql
@@ -1,1 +1,8 @@
+CREATE OR REPLACE VIEW customer_first_order AS
+SELECT
+    customer_id,
+    MIN(order_date::date) AS first_order_date
+FROM project2_orders
+WHERE order_status = 'paid'
+GROUP BY customer_id;
 
